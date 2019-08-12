8b9a093774eb6601054017de5a7aa6cb883c5f0e
Abhishek kale <zabhishekkale@gmail.com>
2019-08-12 10:23:44 +0530

    added all fiels

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const addbtn = document.querySelector('.addb');
 const afrm = document.querySelector(".form");
-addbtn.addEventListener('click',function (){
-afrm.classList.toggle("fadein")
+addbtn.addEventListener('click', function() {
+    afrm.classList.toggle("fadein")
 })
 
-const addbook= ()=>
-{
+const addbook = () => {
     const book = document.createElement("div");
-    const remover= document.createElement("div");
+    const author = document.createElement("div");
+    const title = document.createElement("div");
+    const pages = document.createElement("div");
+    const read = document.createElement("div");
+    const remover = document.createElement("div");
     remover.classList.add("remove");
-    remover.textContent="x";
-    remover.addEventListener('click',()=>{
+    remover.textContent = "x";
+    remover.addEventListener('click', () => {
         book.remove();
     });
-  
+
     const list = document.querySelector(".list");
-    book.textContent=`${document.getElementById("title").value} is written by ${document.getElementById("author").value}`;
+    title.textContent = document.getElementById("title").value;
+    author.textContent = document.getElementById("author").value;
+    pages.textContent = document.getElementById("pages").value;
+    if (document.getElementById("pages").value == 1) {
+        read.textContent = "O";
+    } else {
+        read.textContent = "X";
+    }
     book.classList.add("item");
-    book.appendChild(remover)
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
+    book.appendChild(remover);
     list.appendChild(book);
-    afrm.classList.toggle("fadein")
-    
+    afrm.classList.toggle("fadein");
+
 }
